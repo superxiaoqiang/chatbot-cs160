f06de4cd9f64d0b3c06a07ac2fadea0722edacc1
paul.craciunoiu <paul.craciunoiu@39997754-0b0c-11df-a6e1-f7bfe9b6b944>
2010-03-16 02:08:30 +0000

    Make logging optional and unindent a lot of unnecessary indentation from internalstate.py

--- trunk/chatbot/lib/internalstate.py
+++ trunk/chatbot/lib/internalstate.py
@@ -52,7 +52,8 @@ class InternalState:
         item = self.peek_stack()
         if item and item.get('listmode', None):
             self.last_list_pos = self.count - 1
-            log.debug(self.last_list_pos)
+            if constants.DEBUG:
+                log.debug(self.last_list_pos)
 
         new['raw_input'] = raw_input
         # push raw input to stack
@@ -66,152 +67,155 @@ class InternalState:
         filters = top['filters']
         # get previous item
         prev = self.peek_stack(-2)
-        if top:
-            it = input['type'].split('-')
-            input['list'] = []
+        it = input['type'].split('-')
+        input['list'] = []
 
-            # preprocess confirmation
-            if it[0] == 'confirmation' and prev:
-                try:
-                    log.debug('Previous type: {it}'.format(
-                        it=prev['input']['type'],
-                    ))
-                    # carry over restaurant
-                    if prev['input']['type'] == 'leading-name-detail' \
-                        or prev['input']['type'] == 'name-detail':
-                        input = prev['input']
-                        input['type'] = 'name-detail'
-                        it = input['type'].split('-')
-                except:
-                    pass
-
-            # if it already has context, skip confirmation step
-            if top.get('internal', False) and \
-                it[0] == 'leading' and it[1] == 'name':
-                input['type'] = 'name-detail'
-                it = input['type'].split('-')
-
-            if it[0] == 'undo':
-                popped = False
-                for i in range(0, 2):
-                    item = self.pop_stack()
-                    if not item:
-                        break
-                    elif i == 1:
-                        popped = True
-                item = self.peek_stack()
-                if item:
-                    input = item['input'].copy()
-                    input['undo'] = True
-                    top['input'] = input
-                    log.debug('Filters: {f}'.format(f=item['filters']))
-                    return input
-                elif popped:
-                    input['type'] = 'undo-empty'
-                else:
-                    input['type'] = 'undo-error'
-
-            if it[0] == 'name':
-
-                if prev['filters'].get('Name', False) and \
-                    top.get('internal', False):
-                    r_list = prev['input']['list']
-                    filters = prev['filters']
-
-                elif it[1] in set(['detail', 'phone', 'zone',
-                    'price', 'location' , 'field22']):
-                    filters.update({'Name': input['restaurant']})
-                    r_list = self._xmlparser.get_restaurants(filters)
-
-                else:
-                    r_list = []
-
-                r_len = len(r_list)
-                if r_len == 1:
-                    it[0] = 'single'
-                    input['type'] = '-'.join(it)
-                    input['list'] = r_list
-                elif r_len > 1:
-                    random.shuffle(r_list)
-                    input['list'] = r_list
-                    top['listmode'] = True
-
-            if it[0] == 'list':
-                # check whether to start from previous or from scratch
-                log.debug(input)
-                if prev and prev['input']['type'].split('-')[0] == 'list':
-                    filters.update(prev['filters'])
-                    if it[1] == 'price':
-                        if filters.get('Cost', None):
-                            del filters['Cost']
-                        if filters.get('minPrice', None):
-                            del filters['minPrice']
-                        if filters.get('maxPrice', None):
-                            del filters['maxPrice']
-
-                # price range
-                if it[1] == 'price' and it[2] == 'range':
-                    filters.update({
-                        'minPrice': input['min'],
-                        'maxPrice': input['max'],
-                    })
-
-                # exact price
-                if it[1] == 'price' and it[2] == 'single':
-                    filters.update({
-                        'Cost': input['price'],
-                    })
-
-                # meal
-                if it[1] == 'meal' and it[2] == 'single':
-                    filters.update({
-                        'MealsServed': input['meal'],
-                    })
-
-                input['list'] = self._xmlparser.get_restaurants(filters)
-
-            if it[0] == 'single':
-
-                if it[1] == 'listitem' and self.last_list_pos:
-                    # get the last listmode from stack
-                    stack_list = self.peek_stack(self.last_list_pos)
-                    # update input type
-                    it = stack_list['input']['type'].split('-')
-                    it[0] = 'single'
-                    input['type'] = '-'.join(it)
-
-                    if input['listitem'] > 0 and \
-                        input['listitem'] <= len(stack_list['input']['list']):
-                        # this is the restaurant just picked
-                        r = stack_list['input']['list'][input['listitem']-1]
-                        # update filters accordingly
-                        filters = stack_list['filters']
-                        filters.update({'Key': r['Key'], })
-                        # finally, update the input
-                        input['list'] = [r]
-
-                elif it[1] == 'listitem':
-                    input['type'] = 'nomatch'
-                    del input['listitem']
-
-                elif it[1] == 'cuisine':
-                    filters.update({'Cuisine': input['cuisine']})
-                    r_list = self._xmlparser.get_restaurants(filters)
-                    random.shuffle(r_list)
-                    input['list'] = [r_list[0]]
-
-            # update the stack with
+        # preprocess confirmation
+        if it[0] == 'confirmation' and prev:
             if constants.DEBUG:
-                log.debug(
-                    'just added: {type}; n: {n}'.format(
-                        type=input['type'],
-                        n=len(input['list']),
-                        )
+                log.debug('Previous type: {it}'.format(
+                    it=prev['input']['type'],
+                ))
+            try:
+                # carry over restaurant
+                if prev['input']['type'] == 'leading-name-detail' \
+                    or prev['input']['type'] == 'name-detail':
+                    input = prev['input']
+                    input['type'] = 'name-detail'
+                    it = input['type'].split('-')
+            except:
+                pass
+
+        # if it already has context, skip confirmation step
+        if top.get('internal', False) and \
+            it[0] == 'leading' and it[1] == 'name':
+            input['type'] = 'name-detail'
+            it = input['type'].split('-')
+
+        if it[0] == 'undo':
+            popped = False
+            for i in range(0, 2):
+                item = self.pop_stack()
+                if not item:
+                    break
+                elif i == 1:
+                    popped = True
+            item = self.peek_stack()
+            if item:
+                input = item['input'].copy()
+                input['undo'] = True
+                top['input'] = input
+                if constants.DEBUG:
+                    log.debug('Filters: {f}'.format(f=item['filters']))
+                return input
+            elif popped:
+                input['type'] = 'undo-empty'
+            else:
+                input['type'] = 'undo-error'
+
+        if it[0] == 'name':
+
+            if prev['filters'].get('Name', False) and \
+                top.get('internal', False):
+                r_list = prev['input']['list']
+                filters = prev['filters']
+
+            elif it[1] in set(['detail', 'phone', 'zone',
+                'price', 'location' , 'field22']):
+                filters.update({'Name': input['restaurant']})
+                r_list = self._xmlparser.get_restaurants(filters)
+
+            else:
+                r_list = []
+
+            r_len = len(r_list)
+            if r_len == 1:
+                it[0] = 'single'
+                input['type'] = '-'.join(it)
+                input['list'] = r_list
+            elif r_len > 1:
+                random.shuffle(r_list)
+                input['list'] = r_list
+                top['listmode'] = True
+
+        if it[0] == 'list':
+            # check whether to start from previous or from scratch
+            if constants.DEBUG:
+                log.debug(input)
+            if prev and prev['input']['type'].split('-')[0] == 'list':
+                filters.update(prev['filters'])
+                if it[1] == 'price':
+                    if filters.get('Cost', None):
+                        del filters['Cost']
+                    if filters.get('minPrice', None):
+                        del filters['minPrice']
+                    if filters.get('maxPrice', None):
+                        del filters['maxPrice']
+
+            # price range
+            if it[1] == 'price' and it[2] == 'range':
+                filters.update({
+                    'minPrice': input['min'],
+                    'maxPrice': input['max'],
+                })
+
+            # exact price
+            if it[1] == 'price' and it[2] == 'single':
+                filters.update({
+                    'Cost': input['price'],
+                })
+
+            # meal
+            if it[1] == 'meal' and it[2] == 'single':
+                filters.update({
+                    'MealsServed': input['meal'],
+                })
+
+            input['list'] = self._xmlparser.get_restaurants(filters)
+
+        if it[0] == 'single':
+
+            if it[1] == 'listitem' and self.last_list_pos:
+                # get the last listmode from stack
+                stack_list = self.peek_stack(self.last_list_pos)
+                # update input type
+                it = stack_list['input']['type'].split('-')
+                it[0] = 'single'
+                input['type'] = '-'.join(it)
+
+                if input['listitem'] > 0 and \
+                    input['listitem'] <= len(stack_list['input']['list']):
+                    # this is the restaurant just picked
+                    r = stack_list['input']['list'][input['listitem']-1]
+                    # update filters accordingly
+                    filters = stack_list['filters']
+                    filters.update({'Key': r['Key'], })
+                    # finally, update the input
+                    input['list'] = [r]
+
+            elif it[1] == 'listitem':
+                input['type'] = 'nomatch'
+                del input['listitem']
+
+            elif it[1] == 'cuisine':
+                filters.update({'Cuisine': input['cuisine']})
+                r_list = self._xmlparser.get_restaurants(filters)
+                random.shuffle(r_list)
+                input['list'] = [r_list[0]]
+
+        # update the stack with
+        if constants.DEBUG:
+            log.debug(
+                'just added: {type}; n: {n}'.format(
+                    type=input['type'],
+                    n=len(input['list']),
                     )
+                )
 
         top['input'] = input
         top['filters'] = filters
-        log.debug('Filters: {f}'.format(f=filters))
+        if constants.DEBUG:
+            log.debug('Filters: {f}'.format(f=filters))
         return input
 
     def reset_stack(self):
